2d49931f4eb19f2c45cb10664c072716a137d210
Yashvi’s Alpha <104771239+NotReallyAlpha@users.noreply.github.com>
2022-08-04 10:33:22 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -2,6 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 import os
 from Database import *
+from YashuForever import broadcast
 
 API_ID = int(os.environ['API_ID'])
 API_HASH = os.environ['API_HASH']
